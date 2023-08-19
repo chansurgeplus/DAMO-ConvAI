01965df051d5c8488d2fa1ea3a92e196c669c2db
Chandeepa Dissanayake <134255133+chansurgeplus@users.noreply.github.com>
2023-08-19 14:56:35 +0000

    Added gzip reading.

--- PRO/train/hh_preprocess_data/step_1_process.py
+++ PRO/train/hh_preprocess_data/step_1_process.py
@@ -3,6 +3,7 @@ import re
 import pprint
 import os
 import tqdm
+import gzip
 
 def gen_global_index():
     index = 0
@@ -16,7 +17,7 @@ Roles = {
 }
 
 def hhrlhf_preprocess(path,filename,index_generator,split='train'):
-    with open(os.path.join(path,filename),'r', encoding='utf-8') as f:
+    with gzip.open(os.path.join(path,filename),'r', encoding='utf-8') as f:
         raw = f.readlines()
     
     data = []
